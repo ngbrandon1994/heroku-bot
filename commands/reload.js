db890c575260caac2614c5b93dc7b91d18dc39b7
Brandon Ng <ngbrandon1994@users.noreply.github.com>
2020-10-16 12:06:34 -0400

    Update reload.js
    Enable the ability to reload a command by using their aliases alongside their command name.

--- commands/reload.js
+++ commands/reload.js
@@ -1,13 +1,36 @@
-exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
+/**
+ * Reloads commands when its updated, without restarting the bot by entering the name of the command to reload as the args.
+ */
+
+exports.run = async (client, message, args, level) => {
   if (!args || args.size < 1) return message.reply("Must provide a command to reload. Derp.");
+  try {
+    let response = await client.unloadCommand(args[0]);
+    if (response) return message.reply(`Error Unloading: ${response}`);
 
-  let response = await client.unloadCommand(args[0]);
-  if (response) return message.reply(`Error Unloading: ${response}`);
+    response = client.loadCommand(args[0]);
+    if (response) return message.reply(`Error Loading: ${response}`);
 
-  response = client.loadCommand(args[0]);
-  if (response) return message.reply(`Error Loading: ${response}`);
+    return message.reply(`The command \`${args[0]}\` has been reloaded`);
+  } catch (e){
 
-  message.reply(`The command \`${args[0]}\` has been reloaded`);
+    try {
+      const cmd = client.commands.get(args[0]) || client.commands.get(client.aliases.get(args[0]));
+      if (!cmd) throw `Can't find Command File`;
+      cmdName = cmd.help.name;
+      let response = await client.unloadCommand(cmdName);
+      if (response) return message.reply(`Error Unloading: ${response}`);
+  
+      response = client.loadCommand(cmdName);
+      if (response) return message.reply(`Error Loading: ${response}`);
+  
+      return message.reply(`The command \`${args[0]}\` has been reloaded`);
+    }catch (e){
+      console.log(e);
+      return message.reply(`The command \`${args[0]}\` does not exist! Try again by using its filename!`);
+    }
+
+  }
 };
 
 exports.conf = {
